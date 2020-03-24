277bd362faf23a853aa4404cf8596089bb439bd5
Julaganti <bhargavajulaganti@quickenloans.com>
2020-03-23 20:58:54 -0400

    added http call and mapped to html for deadlift component

--- my-app/src/app/deadlift/deadlift.component.ts
+++ my-app/src/app/deadlift/deadlift.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, finalize, map } from 'rxjs/operators';
+import { GetData } from './deadlift.response';
 
 @Component({
   selector: 'app-deadlift',
@@ -10,15 +12,36 @@ export class DeadliftComponent implements OnInit {
 
   private static readonly hostName = "https://q6rrg5mw2k.execute-api.us-east-2.amazonaws.com/default/";
 
+
+  deadliftHistory: any;
+  mydata: GetData;
+  Count: number;
+  Items: Array<any>;
   constructor(private http: HttpClient) { }
+
+  // ngOnInit() {
+
+  //   this.deadliftHistory = this.http.get<string>(`${DeadliftComponent.hostName}/getworkouthistory`)
+  //     .pipe().subscribe((data: any) => {
+  //       console.log(data.Count);
+  //       console.log(JSON.stringify(data));
+  //     });
+  // }
+
 
   ngOnInit() {
 
-    this.http.get<string>(`${DeadliftComponent.hostName}/getworkouthistory`)
-      .pipe().subscribe( (data: string) => {
-        console.log(JSON.stringify(data));
-      });
-
+    this.deadliftHistory = this.http.get<GetData>(`${DeadliftComponent.hostName}/getworkouthistory`)
+      .pipe(map(data => {
+        if(data){
+          this.deadliftHistory = JSON.stringify(data);
+          this.Count = data.Count;
+          this.Items = data.Items;
+          console.log(this.Count);
+          console.log(this.Items[0].CreatedDate);
+          console.log(this.Items[0].Set1);
+          console.log(this.Items[1].CreatedDate);
+        }
+      })).subscribe();
   }
-
 }
